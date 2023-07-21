efeb82317110f6c39335f717212328c0c15fb9b0
熊梓云 <414992668@qq.com>
2023-07-21 16:02:18 +0800

    1

--- init.js
+++ init.js
@@ -1,26 +1,68 @@
-const fs = require("fs");
+const fs = require('fs');
+const path = require('path');
+const arr = []
+function readFiles(dir, callback) {
+  let completed = 0;
+  fs.readdir(dir, (err, files) => {
+    if (err) {
+      return callback(err);
+    }
 
-// 目标文件夹的路径
-const folderPath = "./source"; // 替换为目标文件夹的路径
+    if (files.length === 0) {
+      // 处理空文件夹的情况
+      return callback();
+    }
 
-// 读取目标文件夹的内容
-fs.readdir(folderPath, (err, files) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
-  const arr = files.filter((v) => v.endsWith(".mp3") || v.endsWith(".flac"));
+    files.forEach(file => {
+      const filePath = path.join(dir, file);
+      fs.stat(filePath, (err, stat) => {
+        if (err) {
+          return callback(err);
+        }
+
+        if (stat.isFile()) {
+          // 处理文件
+          if(filePath.endsWith(".mp3") || filePath.endsWith(".wav") || filePath.endsWith(".flac")) {
+            arr.push(filePath)
+          }
+          completed++;
+          if (completed === files.length) {
+            // 所有文件处理完成
+            callback();
+          }
+        } else if (stat.isDirectory()) {
+          // 处理子文件夹
+          readFiles(filePath, err => {
+            if (err) {
+              return callback(err);
+            }
+            completed++;
+            if (completed === files.length) {
+              // 所有文件处理完成
+              callback();
+            }
+          });
+        }
+      });
+    });
+  });
+}
+
+
+function mkdirJson() {
   // 处理文件夹中的文件列表
   const jsonData = arr.map((v) => {
-    const name = v.slice(0, -4);
+    str = v.replace(/\\/g, '/')
+    const lastIndex = str.lastIndexOf('/');
+    const name = str.substring(lastIndex + 1);
     return {
       name,
       singer: "",
       album: "",
       time: "",
-      link_url: `./source/${v}`,
-      cover: `./source/${name}.jpg`,
-      link_lrc: `./source/${name}.txt`,
+      link_url: `./${str}`,
+      cover: "",
+      link_lrc: "",
     };
   });
 
@@ -34,4 +76,15 @@ fs.readdir(folderPath, (err, files) => {
     }
     console.log("JSON 文件已生成");
   });
+}
+
+// 传入需要读取的文件夹路径和回调函数
+const folderPath = './source';
+readFiles(folderPath, err => {
+  if (err) {
+    console.error('读取文件时出错:', err);
+  } else {
+    console.log('文件读取完成');
+    mkdirJson()
+  }
 });
